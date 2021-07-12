1aa11737aa5d8d8f8b2e2568880b6127bd54c258
pocachp <pocachip0318@gmail.com>
2021-07-12 19:07:51 +0900

    JS Home contact button

--- main.js
+++ main.js
@@ -24,10 +24,20 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null){
         return;
     }
-    console.log(event.target.dataset.link); 
-    
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
-
-
+    scrollIntoView(link);
 });
+
+
+// Handle click on "contact me" buttoon on home
+
+const homeContactBtn = document.querySelector(".home__contact");
+
+homeContactBtn.addEventListener('click', () => {
+   scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
+//반복되어 사용되므로 편리하게 함수 선언
